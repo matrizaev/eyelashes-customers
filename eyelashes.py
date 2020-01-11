76493eb8fb4ce33063bce38a93c7069be52efe29
matrizaev <matrizaev@gmail.com>
2020-01-11 20:31:14 +0000

    Sat Jan 11 20:31:14 UTC 2020

--- eyelashes.py
+++ eyelashes.py
@@ -1,8 +1,8 @@
 from app import create_app, db
-from app.models import User, Placemark, Tag, Subtag, SubtagPlacemark
+from app.models import User, Customer
 
 application = create_app()
 
 @application.shell_context_processor
 def make_shell_context():
-	return {'db': db, 'User': User, 'Placemark' : Placemark, 'Tag':Tag, 'Subtag':Subtag, 'SubtagPlacemark':SubtagPlacemark}
+	return {'db': db, 'User': User, 'Customer' : Customer}
